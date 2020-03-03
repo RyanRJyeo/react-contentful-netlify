f280bace29e519fbd6049e8fe06f7a31bef1df5d
RyanRJyeo <ryanrj@hotmail.sg>
2020-03-03 18:15:07 +0800

    edit inquiry form again

--- src/pages/Inquiry.js
+++ src/pages/Inquiry.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
+
 const encode = (data) => {
   return Object.keys(data)
       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
       .join("&");
 }
 
-export default class Inquiry extends React.Component {
+class Inquiry extends React.Component {
   constructor(props) {
     super(props);
     this.state = { name: "", email: "", message: "" };
   }
+
+  /* Here’s the juicy bit for posting the form submission */
 
   handleSubmit = e => {
     fetch("/", {
@@ -26,29 +29,44 @@ export default class Inquiry extends React.Component {
     this.setState({ name: "", email: "", message: "" });
   };
 
-
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
 
   render() {
     const { name, email, message } = this.state;
     return (
-      <div>
-        <form className="inquiry-form" onSubmit={this.handleSubmit}>
-          <p>
-            <input className="inquiry-input" type="text" name="name" value={name} onChange={this.handleChange} placeholder="Name" required />
-          </p>
-          <p>
-            <input className="inquiry-input" type="email" name="email" value={email} onChange={this.handleChange} placeholder="Email" required />
-          </p>
-          <p>
-            <textarea className="inquiry-input" type="text" name="message" value={message} onChange={this.handleChange} placeholder="Talk to us!" rows="5" cols="50" required />
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
+      <form className="inquiry-form" onSubmit={this.handleSubmit}>
+        <p>
+          <input className="inquiry-input" type="text" name="name" placeholder="Name" value={name} onChange={this.handleChange} required/>
+        </p>
+        <p>
+          <input className="inquiry-input" type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange} required/>
+        </p>
+        <p>
+          <textarea className="inquiry-input" name="message" placeholder="Talk to us!" value={message} onChange={this.handleChange} rows="5" cols="50" required/>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     );
-  };
-};
+  }
+}
+
+export default Inquiry;
+
+
+
+        // <form className="inquiry-form" onSubmit={this.handleSubmit}>
+        //   <p>
+        //     <input className="inquiry-input" type="text" name="name" value={name} onChange={this.handleChange} placeholder="Name" required />
+        //   </p>
+        //   <p>
+        //     <input className="inquiry-input" type="email" name="email" value={email} onChange={this.handleChange} placeholder="Email" required />
+        //   </p>
+        //   <p>
+        //     <textarea className="inquiry-input" type="text" name="message" value={message} onChange={this.handleChange} placeholder="Talk to us!" rows="5" cols="50" required />
+        //   </p>
+        //   <p>
+        //     <button type="submit">Send</button>
+        //   </p>
+        // </form>
