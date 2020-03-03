c9a1dfba2181db5fc6a0c4c70ae8e7b678091f76
RyanRJyeo <ryanrj@hotmail.sg>
2020-03-03 17:32:53 +0800

    edit inquiry form

--- src/pages/Inquiry.js
+++ src/pages/Inquiry.js
@@ -1,18 +1,48 @@
 import React from 'react';
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 export default class Inquiry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+
+    this.setState({ name: "", email: "", message: "" });
+  };
+
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+
   render() {
+    const { name, email, message } = this.state;
     return (
       <div>
-        <form className="inquiry-form" name="contact" action="POST" data-netlify="true">
+        <form className="inquiry-form" onSubmit={this.handleSubmit}>
           <p>
-            <input className="inquiry-input" type="text" name="name" placeholder="Name" required />
+            <input className="inquiry-input" type="text" name="name" value={name} onChange={this.handleChange} placeholder="Name" required />
           </p>
           <p>
-            <input className="inquiry-input" type="email" name="email" placeholder="Email" required />
+            <input className="inquiry-input" type="email" name="email" value={email} onChange={this.handleChange} placeholder="Email" required />
           </p>
           <p>
-            <textarea className="inquiry-input" type="text" name="description" placeholder="Talk to us!" rows="5" cols="50" required />
+            <textarea className="inquiry-input" type="text" name="message" value={message} onChange={this.handleChange} placeholder="Talk to us!" rows="5" cols="50" required />
           </p>
           <p>
             <button type="submit">Send</button>
